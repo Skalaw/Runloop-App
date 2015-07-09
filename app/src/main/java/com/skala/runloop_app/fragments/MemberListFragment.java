587a5271ff57261fe492c9adef5147eda24346f9
Skalaw <mskalski6@wp.pl>
2015-07-09 21:59:28 +0200

    [ADD] New library: jsoup 1.8.2 [ADD] Download html from: http://runloop.pl/ [ADD] Parse members and save to model

--- app/src/main/java/com/skala/runloop_app/fragments/MemberListFragment.java
+++ app/src/main/java/com/skala/runloop_app/fragments/MemberListFragment.java
@@ -1,8 +1,10 @@
 package com.skala.runloop_app.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,12 @@ import com.skala.runloop_app.MemberAdapter;
 import com.skala.runloop_app.MemberModel;
 import com.skala.runloop_app.R;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -32,14 +40,53 @@ public class MemberListFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        MemberModel memberModel = new MemberModel("Test1", "Test11", "Test111", "Test1111");
-        MemberModel memberModel2 = new MemberModel("Test2", "Test22", "Test222", "Test2222");
-
-        ArrayList<MemberModel> memberList = new ArrayList<>();
-        memberList.add(memberModel);
-        memberList.add(memberModel2);
-
-        MemberAdapter memberAdapter = new MemberAdapter(getActivity(), memberList);
-        mListView.setAdapter(memberAdapter);
+        downloadMemberList();
     }
+
+    private void downloadMemberList() {
+        new AsyncTask<Void, Void, ArrayList<MemberModel>>() {
+            @Override
+            protected ArrayList<MemberModel> doInBackground(Void... params) {
+                ArrayList<MemberModel> memberList = new ArrayList<>();
+                String stringUrl = "http://runloop.pl/";
+
+                try {
+                    Document doc = Jsoup.connect(stringUrl).get();
+                    Elements staffListContainer = doc.getElementsByClass("staff-list-container");
+                    Elements members = staffListContainer.get(0).getElementsByClass("figure");
+
+                    int sizeMember = members.size();
+                    for (int i = 0; i < sizeMember; i++) {
+                        Element member = members.get(i);
+                        String imageUrl = member.getElementsByClass("figure-image").get(0).select("img").get(0).absUrl("src");
+                        String description = member.getElementsByClass("figure-caption-description").get(0).text();
+
+                        Element title = member.getElementsByClass("figure-caption-title").get(0);
+                        String fullName = title.select("strong").text();
+                        String position = title.select("span").text();
+
+                        /*Log.i("MemberList", i + " - " + imageUrl);
+                        Log.i("MemberList", i + " - " + fullName);
+                        Log.i("MemberList", i + " - " + position);
+                        Log.i("MemberList", i + " - " + description);*/
+
+                        MemberModel memberModel = new MemberModel(imageUrl, fullName, position, description);
+                        memberList.add(memberModel);
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return memberList;
+            }
+
+            @Override
+            protected void onPostExecute(ArrayList<MemberModel> memberList) {
+                MemberAdapter memberAdapter = new MemberAdapter(getActivity(), memberList);
+                mListView.setAdapter(memberAdapter);
+            }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
 }
